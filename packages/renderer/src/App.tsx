4413e505d9cfdc7971e947e05ee0517aaf91318c
Kexin Li <hollalikexin@gmail.com>
2022-12-10 21:50:00 +0800

    add tailwind setup

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,48 +1,7 @@
-import {useState} from 'react';
-import Count from '/@/components/Count';
-import {sha256sum, versions} from '#preload';
-
-const APP_VERSION = import.meta.env.VITE_APP_VERSION;
-
 export default function App() {
-  const [rawValue, setRawValue] = useState('hello world');
-
   return (
-    <div>
-      <h2>App</h2>
-      <Count />
-      <br />
-      <div>App version: {APP_VERSION}</div>
-      <ul id="process-versions">
-        {Object.entries(versions).map(([lib, version], idx) => {
-          return (
-            <li key={idx}>
-              <strong>{lib}</strong>
-              <span>: v{version}</span>
-            </li>
-          );
-        })}
-      </ul>
-      <br />
-      <label>
-        Raw value
-        <input
-          type="text"
-          value={rawValue}
-          onChange={e => {
-            setRawValue(e.target.value);
-          }}
-        />
-      </label>
-      <br />
-      <label>
-        Hashed by node:crypto
-        <input
-          type="text"
-          disabled
-          value={sha256sum(rawValue)}
-        />
-      </label>
+    <div className="mx-auto my-8 mt-10 w-8/12 rounded border border-gray-200 p-4 shadow-md dark:border-neutral-600 dark:bg-neutral-800 dark:shadow-none">
+      123
     </div>
   );
 }
--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -1,11 +1,12 @@
-// import {createApp} from 'vue';
-// import App from '/@/App.vue';
-
-// createApp(App).mount('#app');
-
-import * as React from 'react';
+import {StrictMode} from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('app') as Element);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
