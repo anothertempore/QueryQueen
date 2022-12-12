16b7a44cae034d4791dd32f65241a1a9693b7c06
Kexin Li <hollalikexin@gmail.com>
2022-12-12 23:03:44 +0800

    add basic layout

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,14 +1,19 @@
 export default function App() {
   return (
-    <div>
-      <div className="draggable-header">Placeholder</div>
-      <main className="flex m-2">
-        <nav className="flex-1">
-          <div className="">DB</div>
-          <div className="">Nav</div>
-        </nav>
-        <div className="flex-4 content">Content123</div>
-      </main>
+    <div className="h-full">
+      <div className="flex h-full">
+        <div className="w-40">
+          <div className="h-10 text-right flex draggable-area border-b-2 border-indigo-500">
+            Arrow
+          </div>
+        </div>
+        <div className="draggable-area border-r-2 border-indigo-500" />
+        <div className="flex-1">
+          <div className="h-10 text-right flex draggable-area border-b-2 border-indigo-500">
+            Arrow
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
